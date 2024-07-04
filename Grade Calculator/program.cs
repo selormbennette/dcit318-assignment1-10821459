de43fca336e55f44e735f68415931c6ff4df5c84
selormbennette <bsbobie@st.ug.edu.gh>
2024-07-04 13:46:46 +0100

    Grade Calculator Done

---
+++ Grade Calculator/program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Grade_Calculator
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grade Calculator");
+            Console.Write("Enter the numerical grade (0-100): ");
+            double grade = Convert.ToDouble(Console.ReadLine());
+
+                if (grade >= 90 && grade <= 100)
+            {
+                Console.WriteLine("Letter Grade: A");
+            }
+                else if (grade >= 80 && grade <= 89)
+            {
+                Console.WriteLine("Letter Grade: B");
+            }
+                else if (grade >= 70 && grade <= 79)
+            {
+                Console.WriteLine("Letter Grade: C");
+            }
+                else if (grade >= 60 && grade <= 69)
+            {
+                Console.WriteLine("Letter Grade: D");
+            }
+                else if (grade < 60 && grade >= 0)
+            {
+                Console.WriteLine("Letter Grade: F");
+            }
+                else
+            {
+                Console.WriteLine("Invalid grade entered.");
+            }
+        }
+    }
+}
